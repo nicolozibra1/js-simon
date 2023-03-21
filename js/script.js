c26b2adf1e149302532988aaee2305f76e6da9e0
nicolozibra1 <nico.zibra@gmail.com>
2023-03-21 15:40:14 +0100

    add-cycle(for)-check-match

--- js/script.js
+++ js/script.js
@@ -18,22 +18,33 @@ generate.addEventListener('click', function () {
     console.log(numbers);
     const boxNumber = document.querySelector('.box-number');
     boxNumber.innerHTML = `<h1 class="text-white text-center">${numbers}</h1>`;
+
+    const btnSend = document.getElementById('form');
+    btnSend.addEventListener('submit', function (e){
+        e.preventDefault();
+        let inputs = [
+            parseInt(document.getElementById('n1').value),
+            parseInt(document.getElementById('n2').value),
+            parseInt(document.getElementById('n3').value),
+            parseInt(document.getElementById('n4').value),
+            parseInt(document.getElementById('n5').value)
+            ]
+            console.log(inputs);
+    
+        let count = 0;
+        let userNumber = [];
+
+        for(let i = 0; i < inputs.length; i++) {
+            if(numbers.includes(inputs[i])){
+                count++;
+                userNumber.push(inputs[i]);
+                console.log(inputs[i]);
+            }
+        }
+    });
 });
 
-const result = document.querySelector('.box-result');
-
-const btnSend = document.getElementById('form');
-btnSend.addEventListener('submit', function (e){
-    e.preventDefault();
-    let inputs = [
-        parseInt(document.getElementById('n1').value),
-        parseInt(document.getElementById('n2').value),
-        parseInt(document.getElementById('n3').value),
-        parseInt(document.getElementById('n4').value),
-        parseInt(document.getElementById('n5').value)
-        ]
-        console.log(inputs);
-});
+    
 
 
 

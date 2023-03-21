9f3f6760507d733a6a26d59d0aa675debf5cdd36
nicolozibra1 <nico.zibra@gmail.com>
2023-03-21 16:25:18 +0100

    fix-input-d-none

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-
-
-
 // FUNZIONE PER GENERARE I NUMERI
 const generate = document.getElementById('generate');
 let timer = 0;
 generate.addEventListener('click', function () {
     timer = setTimeout(function () {
     boxNumber.innerHTML ='';
+    btnSend.classList.toggle('d-none');
     }, 10000);
 
     const numbers = [];
@@ -22,7 +20,7 @@ generate.addEventListener('click', function () {
     console.log(numbers);
     let boxNumber = document.querySelector('.box-number');
     boxNumber.innerHTML = `<h1 class="text-white text-center">${numbers}</h1>`;
-
+    // PRELEVO I NUMERI IN INPUT DEL UTENTE
     const btnSend = document.getElementById('form');
     btnSend.addEventListener('submit', function (e){
         e.preventDefault();
@@ -37,7 +35,7 @@ generate.addEventListener('click', function () {
     
         let count = 0;
         let userNumber = [];
-
+        // VERIFICO LA CORRISPONDEZA TRA NUMERI BOT E NUMERI UTENTE
         for(let i = 0; i < inputs.length; i++) {
             if(numbers.includes(inputs[i])){
                 count++;

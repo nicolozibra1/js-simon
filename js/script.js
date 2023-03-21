cf0bf86fca415218ffa312402d667be5886628b4
nicolozibra1 <nico.zibra@gmail.com>
2023-03-21 23:46:43 +0100

    add-countdown

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
-// FUNZIONE PER GENERARE I NUMERI
+// FUNZIONE GENERA
 const generate = document.getElementById('generate');
-let timer = 0;
 generate.addEventListener('click', function () {
+     // CONTO ALLA ROVESCIA
+    const countdown = document.querySelector('.countdown');
+    let message = document.querySelector('.i-message');
+    let count = 10;
+    countdown.innerHTML = `<h4>Tempo rimanente:</h4>
+                           <h4>${count}</h4>`;
+    const endTime = setInterval(() => {
+     if(count === 1) {
+         clearInterval(endTime);
+         countdown.classList.add('d-none');
+         message.classList.remove('d-none');
+     }
+     else {
+         count--;
+     }  
+     countdown.innerHTML = `<h4>Tempo rimanente:</h4>
+                            <h4>${count}</h4>`;
+     console.log(count);
+ }, 1000);
+
+    //  TIMER INVISIBILITA
+    let timer = 0;
     timer = setTimeout(function () {
     boxNumber.innerHTML ='';
     btnSend.classList.toggle('d-none');
     }, 10000);
 
+    // GENERAZIONE NUMERI
     const numbers = [];
-    
+
     while(numbers.length < 5) {
         const number = getRndNumber(1, 100);
-    
+
         if(!numbers.includes(number)) {
         numbers.push(number);
         }
@@ -20,6 +42,7 @@ generate.addEventListener('click', function () {
     console.log(numbers);
     let boxNumber = document.querySelector('.box-number');
     boxNumber.innerHTML = `<h1 class="text-white text-center">${numbers}</h1>`;
+
     // PRELEVO I NUMERI IN INPUT DEL UTENTE
     function checkNumber(e) {
         e.preventDefault();
@@ -31,7 +54,7 @@ generate.addEventListener('click', function () {
             parseInt(document.getElementById('n5').value)
             ]
             console.log(inputs);
-    
+
         let count = 0;
         let userNumber = [];
         // VERIFICO LA CORRISPONDEZA TRA NUMERI BOT E NUMERI UTENTE
@@ -53,14 +76,14 @@ generate.addEventListener('click', function () {
 
         const btnSend = document.getElementById('form');
         btnSend.addEventListener('submit', checkNumber);
-       
+
         const restart = document.getElementById('restart');
         restart.addEventListener('click', function() {
             location.reload();
         });
     });
 
-    
+
 
 
 
